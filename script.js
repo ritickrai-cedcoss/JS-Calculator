f8cae37c7a4b75e25426eac639b5683d70ed7845
ritickrai-cedcoss <ritickrai@cedcoss.com>
2022-03-21 12:34:47 +0530

    created the calculator program

---
+++ script.js
@@ -0,0 +1,25 @@
+var container;
+function allClear() {
+  document.getElementById("displayValue").value = "";
+}
+
+var screen = document.getElementById("displayValue");
+buttons = document.querySelectorAll("button");
+var screenValue = "";
+for (item of buttons) {
+  item.addEventListener("click", (e) => {
+    buttonText = e.target.innerText;
+    if (buttonText == "X") {
+      buttonText = "*";
+      screenValue += buttonText;
+      screen.value = screenValue;
+    } else if (buttonText == "AC"){
+        allClear();
+    } else if (buttonText == "=") {
+      screen.value = eval(screenValue);
+    } else {
+      screenValue += buttonText;
+      screen.value = screenValue;
+    }
+  });
+}
